c0de804d08fa358f5bd9bed11db08289f872a2d1
Paweł <pablos.lukowski@wp.pl>
2017-06-06 00:01:19 +0200

    Initial commit

---
+++ sentEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    $name = $_POST['name']?? 'null';
+    $surname = $_POST['surname']?? 'null';
+    $numberPeople = $_POST['numberPeople']?? 'null';
+    $phone = $_POST['phone']?? 'null';
+    $dateStart = $_POST['dateStart']?? 'null';
+    $dateEnd = $_POST['dateEnd']?? 'null';
+
+    $arrayTranlstate = ['Imie', 'Nazwisko', 'Ilość Osób', 'Telefon', 'DataRozpoczcia','DataZakonczenia'];
+    $message = null;
+    $array = array_values($_POST);
+    foreach ($array as $key => $item) {
+        $message .= $arrayTranlstate[$key] . ' - ' . $item . "<br>";
+
+    }
+
+    if ($dateStart > $dateEnd)
+        echo json_encode(['messageType' => 'danger']);
+    else {
+
+        try{
+            $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+                ->setUsername('kerrallla1@gmail.com') //twoj email do konta gmail
+                ->setPassword('kolowrot313122@'); //haslo do konta gmail
+            $mailer = Swift_Mailer::newInstance($transport);
+
+            $message = Swift_Message::newInstance('Tytuł')
+                ->setFrom('pablos.lukowski@wp.pl')
+                ->setTo(array('pablos.lukowski@wp.pl')) //do kogo beda szly maile.. po przecinku mozna podać wiele emaili
+                ->setBody($message, 'text/html');
+
+            if ($mailer->send($message)) {
+                header('Content-type:application/json;charset=utf-8');
+                echo json_encode(['messageType' => 'success','message'=>'wysłano']);
+            }
+        }catch(\Swift_TransportException $e){
+            echo json_encode(['messageType' => 'danger','message'=>'błąd']);
+        }
+
+    }
+
+}
